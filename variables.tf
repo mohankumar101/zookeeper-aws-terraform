0ce3a2f477c6a257af6b170cd3565e19e9fa8a1e
MohanKumar A V <mohankumar.official@gmail.com>
2024-02-06 14:08:12 +0000

    Modified the way variables are defined

--- variables.tf
+++ variables.tf
@@ -1,3 +1,28 @@
+/* Replaced the bottom section of specifying invidual variable definitions into a map of objects, which can then be used for multiple deployments (Dev/QA/PROD releases) */
+
+variable "serverconfig" {
+    description = "Zookeeper EC2 instance configuration"
+    type = map(object({
+        zk-region = string
+        zk_ec2_inst_type = string
+        zk_inst_name = set(string)
+        zk_ec2_key_name = string
+        zk-subnets = list(string)
+  }))
+
+    default = {
+        test = {
+            zk-region = "us-west-2a"
+            zk_ec2_inst_type = "t2.micro"
+            zk_inst_name = ["member1", "member2"]
+            zk_ec2_key_name = "avmk-newkeys"
+            zk-subnets = ["10.0.1.0/24","10.0.2.0/24"]
+        }
+    }
+}
+
+
+/*
 variable "zk-region" {
     type = string
     default = "us-west-2a"
@@ -8,15 +33,14 @@ variable "zk_ec2_inst_type" {
     type = string
     default = "t2.micro"
 }
-
 variable "zk_ec2_key_name" {
     description = "Zookeeper SSH Keys"
     type = string
     default = "avmk-newkeys"
 }
-
 variable "zk-subnets" {
     description = "Zookeeper Subnets"
     type = list(string)
     default = ["10.0.1.0/24","10.0.2.0/24"]
 }
+*/
