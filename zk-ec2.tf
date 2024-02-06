8ba2cb9d01662db25c8368c53426e41c15f747cd
MohanKumar A V <mohankumar.official@gmail.com>
2024-02-06 14:10:08 +0000

    Update zk-ec2.tf
    Modified to include for_each

--- zk-ec2.tf
+++ zk-ec2.tf
@@ -12,15 +12,18 @@ data "aws_ami" "zk-ec2-amidata" {
   }
 }
 
+/* Added for_each meta to iterate over the instance name list and create multiple instances at a time */
+
 resource "aws_instance" "zk_ec2" {
-  ami           = data.aws_ami.zk-ec2-amidata.id
-  instance_type = var.zk_ec2_inst_type
-  user_data     = ""
-  key_name      = var.zk_ec2_key_name
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_zk_traffic.id ]
-  subnet_id = aws_subnet.zk-public-subnet.id
-  associate_public_ip_address = true
-  tags = {
-    Name = "Zookeeper EC2 Member"
+    for_each = var.serverconfig.test.zk_inst_name
+    ami           = data.aws_ami.zk-ec2-amidata.id
+    instance_type = var.serverconfig.test.zk_ec2_inst_type
+    user_data     = ""
+    key_name      = var.serverconfig.test.zk_ec2_key_name
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_zk_traffic.id ]
+    subnet_id = aws_subnet.zk-public-subnet.id
+    associate_public_ip_address = true
+    tags = {
+        Name = "Zookeeper EC2 - ${each.value}"
   }
 }
